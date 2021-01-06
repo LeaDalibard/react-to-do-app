3ea9d121215416e4964fc9ae437eba0f30b29851
Lea Dalibard <leadalibard@gmail.com>
2021-01-06 14:13:12 +0100

    Calling event check both from child to parent

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,7 +5,14 @@ class TodoItem extends React.Component {
 
     render() {
         return (
-            <li><input type="checkbox" checked={this.props.todo.completed} />{this.props.todo.title}</li>
+            <li>
+                <input
+                    type="checkbox"
+                    checked={this.props.todo.completed}
+                    onChange={()=>this.props.handleChangeProps()}
+                />
+                    {this.props.todo.title}
+            </li>
         )
     }
 }
