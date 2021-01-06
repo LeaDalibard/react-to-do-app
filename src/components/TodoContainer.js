cdf9c4259451f19f1c9d2991cec71875a59e94c2
Lea Dalibard <leadalibard@gmail.com>
2021-01-06 11:23:08 +0100

    Render state to Frontend

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -25,12 +25,14 @@ class TodoContainer extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello from Create React App</h1>
-                <p>I am in a React Component!</p>
+                {this.state.todos.map(todo => (
+                    <li>{todo.title}</li>
+                ))}
             </div>
         )
     }
 }
+
 export default TodoContainer
 
 //render => to return the JSX
