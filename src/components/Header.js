e2329b38b77cb1783d3527b0836cba6159d18486
Lea Dalibard <leadalibard@gmail.com>
2021-01-07 10:04:49 +0100

    Adding a method with sentence appearing with random color on click with componentDidUpdate

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,14 @@ import React, { Component }  from "react"
 class Header extends Component {
 
     componentDidUpdate(prevProps, prevState) {
+        var x = Math.floor(Math.random() * 256);
+        var y = Math.floor(Math.random() * 256);
+        var z = Math.floor(Math.random() * 256);
+        var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
         if (prevProps.headerSpan !== this.props.headerSpan) {
-            console.log("props change");
+            document.getElementById("inH1").innerHTML = "clicked";
+            document.getElementById("inH1").style.backgroundColor = bgColor;
         }
     }
 
