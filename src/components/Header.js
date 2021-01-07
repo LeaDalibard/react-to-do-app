b9f47f44f4d0a70137ea6ccbee54c647d2f3a797
Lea Dalibard <leadalibard@gmail.com>
2021-01-07 12:28:58 +0100

    Converting Header to function to use useEffect on updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,31 @@
-import React, { Component }  from "react"
+import React, { useEffect } from "react"
 
-class Header extends Component {
-
-    componentDidUpdate(prevProps, prevState) {
-        var x = Math.floor(Math.random() * 256);
-        var y = Math.floor(Math.random() * 256);
-        var z = Math.floor(Math.random() * 256);
-        var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-
-        if (prevProps.headerSpan !== this.props.headerSpan) {
-            document.getElementById("inH1").innerHTML = "clicked";
-            document.getElementById("inH1").style.backgroundColor = bgColor;
-        }
+const Header = props => {
+    const headerStyle = {
+        padding: "20px 0",
+        lineHeight: "2em",
     }
 
-    render() {
-        const headerStyle = {
-            padding: "20px 0",
-            lineHeight: "2em",
-        }
-        return (
-            <header style={headerStyle}>
-                <h1 style={{ fontSize: "25px", marginBottom: "15px" }}>
-                    Simple Todo App <span id="inH1"></span>
-                </h1>
-                <p style={{ fontSize: "19px" }}>
-                    Please add to-dos item(s) through the input field
-                </p>
-            </header>
-        )
-    }
+    useEffect(() => {
+        var x = Math.floor(Math.random() * 256)
+        var y = Math.floor(Math.random() * 256)
+        var z = Math.floor(Math.random() * 256)
+        var bgColor = "rgb(" + x + "," + y + "," + z + ")"
+
+        document.getElementById("inH1").innerHTML = "clicked"
+        document.getElementById("inH1").style.backgroundColor = bgColor
+    }, [props.headerSpan])
+
+    return (
+        <header style={headerStyle}>
+            <h1 style={{ fontSize: "25px", marginBottom: "15px" }}>
+                Simple Todo App <span id="inH1"></span>
+            </h1>
+            <p style={{ fontSize: "19px" }}>
+                Please add to-dos item(s) through the input field
+            </p>
+        </header>
+    )
 }
+
 export default Header
