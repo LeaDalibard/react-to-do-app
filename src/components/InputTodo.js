874021cfedf9be22eb1820d060aff6966aa4a9ba
Lea Dalibard <leadalibard@gmail.com>
2021-01-07 12:06:38 +0100

    Change class to function with useState with several states

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,24 +1,32 @@
 import React,{ useState } from "react"
 
 const InputTodo =  props => {
-    const [title, setTitle] = useState("")
+    const [inputText, setInputText] = useState({
+        title: "",
+    })
 
     const onChange = e => {
-        setTitle(e.target.value)
+        setInputText({
+            ...inputText,
+            [e.target.name]: e.target.value,
+        })
     }
 
     const handleSubmit = e => {
         e.preventDefault()
-        props.addTodoProps(title)
-        setTitle("")
+        props.addTodoProps(inputText.title)
+        setInputText({
+            title: "",
+        })
     }
+
     return (
         <form onSubmit={handleSubmit} className="form-container">
             <input
                 type="text"
                 className="input-text"
                 placeholder="Add todo..."
-                value={title}
+                value={inputText.title}
                 name="title"
                 onChange={onChange}
             />
